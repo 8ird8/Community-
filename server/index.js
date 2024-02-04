867df586d2409bae602a3f06ab4ffdf7e07b816c
Achraf <117481103+8ird8@users.noreply.github.com>
2024-02-04 01:57:59 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const { userInfo } = require("os");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["https://community-nu.vercel.app/"],
   methods : ["POST", "GET"],
   credentials: true,
 };
