c29699e4ba96635065bda05141994f191ff13e1b
Achraf <117481103+8ird8@users.noreply.github.com>
2024-02-04 02:45:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const { userInfo } = require("os");
 const app = express();
 
 const corsOptions = {
-  origin: ["https://community-steel.vercel.app/login"],
+  origin: ["https://community-phaw.vercel.app/login"],
   methods : ["POST", "GET"],
   credentials: true,
 };
